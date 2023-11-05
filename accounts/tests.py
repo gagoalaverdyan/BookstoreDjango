1f384410357cab865c179d43eccb51659c6782d6
gagoalaverdyan <gagoalaverdyan@gmail.com>
2023-11-06 00:44:27 +0400

    account management updated, tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
 
 
-# Create your tests here.
 class CustomUserTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
@@ -25,3 +27,24 @@ class CustomUserTests(TestCase):
         self.assertTrue(superuser.is_active)
         self.assertTrue(superuser.is_staff)
         self.assertTrue(superuser.is_superuser)
+
+
+class SignUpPageTests(TestCase):
+    def setUp(self):
+        url = reverse("signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "Mac Miller is the GOAT")
+
+    def test_signup_form(self):
+        form = self.response.context.get("form")
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_signup_view(self):
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
