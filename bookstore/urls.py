7d8c761999442e36d810c35416bee328aafc2174
gagoalaverdyan <gagoalaverdyan@gmail.com>
2023-11-12 00:21:12 +0400

    added cover uploads

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,9 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    # Admin panel
     path("admin/", admin.site.urls),
+    # User management
     path("accounts/", include("allauth.urls")),
+    # Local apps
     path("", include("pages.urls")),
     path("books/", include("books.urls")),
-]
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)  # Media root
